ceb793ff8882f7fa2998dab31d300ba64670df44
denisde4ev <git@denisde4ev.xyz>
2025-06-25 09:35:09 +0300

    c

---
+++ doc-or-something/doc.md
@@ -0,0 +1,7 @@
+
+same as: https://pubs.opengroup.org/onlinepubs/7908799/xcu/tar.html
+"Note that if several files with the same name are in the archive, the last one overwrites all earlier ones"
+
+
+https://github.com/mirror/busybox/blob/371fe9f71d445d18be28c82a2a6d82115c8af19d/archival/tar.c#L740
+https://github.com/landley/toybox/blob/105a72fd53c2fbfd9f66cf23f365b604f016772b/toys/posix/tar.c#L28
--- hrxfile-selfextracting.sh
+++ hrxfile-selfextracting.sh
@@ -5,34 +5,34 @@ case $1 in
 -x) set -x; shift;;
 -*) printf %s\\n >&2 "use --help for usage"; exit 2;;
 esac
-# sed -ne '/^@@ HRX @@$/,$ p' -- "$0" | {
-tail -n+ -- "$0" | {
+sed -ne '/^@@ HRX @@$/,$ p' -- "$0" | {
+#tail -n+ -- "$0" | {
 	read -r _ # ignore line "@@ HRX @@"
 
 	read -r line || case $line in '') exit 1; esac
 	while :; do
 		case $line in
-		"<=> "[!\ ]*/)
-			dirname=${line#"<=> "}
+		"<===> "[!\ ]*/)
+			dirname=${line#"<===> "}
 			mkdir -pv -- ./"$dirname"
 			read -r line
 			;;
-		"<=> "*)
-			fname=${line#"<=> "}
+		"<===> "*)
+			fname=${line#"<===> "}
 			printf %s\\n "file: $fname"
 			case $fname in */*) mkdir -pv -- "${fname%/*}"; esac
 			: >"$fname"
-			while read -r line; do case $line in "<=>"*) continue 2; esac
+			while read -r line; do case $line in "<===>"*) continue 2; esac
 				printf %s\\n "$line" >>./"$fname"
 			done
 			;;
-		"<=>")
-			while read -r line; do case $line in "<=>"*) continue 2; esac
+		"<===>")
+			while read -r line; do case $line in "<===>"*) continue 2; esac
 				printf %s\\n "comment: $line"
 			done
 			;;
 		*)
-			printf %s\\n >&2 "expected to find <=> in line: '$line'"
+			printf %s\\n >&2 "expected to find <===> in line: '$line'"
 			read -r line
 		esac
 
@@ -42,23 +42,24 @@ tail -n+ -- "$0" | {
 
 exit
 
-<=> file-1
+@@ HRX @@
+<===> file-1
 this is the content of file 1
-<=>
+<===>
 this is a comment
-<=>
-<=> file-2
+<===>
+<===> file-2
 this is the content of file 2
-<=> file-3
+<===> file-3
 thi is tha file 3
-<=>
-<=> dir2/
-<=>
-<=> empty-file
-<=>
-<=> dir/
-<=>
-<=> dir/a
+<===>
+<===> dir2/
+<===>
+<===> empty-file
+<===>
+<===> dir/
+<===>
+<===> dir/a
 file a
-<=> dir3/b
+<===> dir3/b
 file b
